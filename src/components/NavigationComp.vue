6ba48f1d70edcb9825e1b420e668b14a73386194
Amanda <aeverett1317@gmail.com>
2019-03-20 10:07:49 -0700

    initial commit'

---
+++ src/components/NavigationComp.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-toolbar>
+    <v-toolbar-side-icon></v-toolbar-side-icon>
+    <v-toolbar-title>Title</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-toolbar-items class="hidden-sm-and-down">
+      <v-btn to="/" flat>Home</v-btn>
+      <v-btn to="/about" flat>About</v-btn>
+      <v-btn flat>Link Three</v-btn>
+    </v-toolbar-items>
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+  name: "NavigationComp"
+};
+</script>
